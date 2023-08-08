6eed4e6b1e9f9abdbf4b64ed9ec3f8aee360b550
Jorge Bejar <jorge@wyeworks.com>
2023-08-08 00:19:56 -0300

    More Home screen sections

--- src/home/home_screen.rs
+++ src/home/home_screen.rs
@@ -14,6 +14,15 @@ live_design! {
     SCAN_ICON = dep("crate://self/resources/scan.png")
     CAMERA_ICON = dep("crate://self/resources/camera.png")
 
+    TAOBAO_COINS_IMG = dep("crate://self/resources/taobao_coins.png")
+    HELP_CENTER_IMG = dep("crate://self/resources/help_center.png")
+    LOGISTICS_IMG = dep("crate://self/resources/logistics_center.png")
+    PAYMENTS_IMG = dep("crate://self/resources/payments_center.png")
+    GLOBAL_DEALS_IMG = dep("crate://self/resources/global_deals.png")
+
+    CREDIT_CARDS_IMG = dep("crate://self/resources/credit_cards.png")
+    SHIPPING_ESTIMATE_IMG = dep("crate://self/resources/shipping_estimate.png")
+
     IconWithText = <Frame> {
         walk: {width: Fit, height: Fit}
         layout: {flow: Down, align: {x: 0.5, y: 0.5}, spacing: 5.0}
@@ -27,6 +36,65 @@ live_design! {
                 color: #000
                 text_style: <REGULAR_TEXT> {font_size: 8.0},
             }
+        }
+    }
+
+    IconWithTwoLineText = <Frame> {
+        walk: {width: Fit, height: Fit}
+        layout: {flow: Down, align: {x: 0.5, y: 0.5}, spacing: 5.0}
+
+        image = <Image> {
+            walk: {width: 40, height: 40}
+        }
+        caption = <Label> {
+            walk: {width: Fit, height: Fit}
+            draw_label: {
+                color: #000
+                text_style: <REGULAR_TEXT> {font_size: 8.0},
+            }
+        }
+        caption2 = <Label> {
+            walk: {width: Fit, height: Fit}
+            draw_label: {
+                color: #000
+                text_style: <REGULAR_TEXT> {font_size: 8.0},
+            }
+        }
+    }
+
+    TopBar = <Frame> {
+        walk: {width: Fill, height: Fit}
+        layout: {flow: Right, align: {x: 0.0, y: 0.5}, spacing: 10.0, padding: {left: 10., right: 10.}}
+
+        <IconWithText> {
+            image = { image: (WORLDWIDE_ICON) }
+            caption = { label: "WW" }
+        }
+
+        <FillerX> {}
+
+        <Label> {
+            walk: {width: Fit, height: Fit}
+            label: "Recommend"
+            draw_label: {
+                color: #000
+                text_style: <REGULAR_TEXT> {},
+            }
+        }
+        <Label> {
+            walk: {width: Fit, height: Fit}
+            label: "Special Offer"
+            draw_label: {
+                color: #000
+                text_style: <REGULAR_TEXT> {},
+            }
+        }
+
+        <FillerX> {}
+
+        <IconWithText> {
+            image = { image: (MEMBERSHIP_CODE_ICON) }
+            caption = { label: "M Code" }
         }
     }
 
@@ -117,7 +185,7 @@ live_design! {
             walk: {width: Fit, height: Fit}
             layout: {
                 flow: Right, align: {x: 0.0, y: 0.5}, spacing: 10.0,
-                padding: {top: 6., left: 6., right: 6., bottom: 6.}
+                padding: {top: 9., left: 6., right: 6., bottom: 9.}
             }
             draw_bg: {
                 color: (ORANGE_COLOR),
@@ -135,6 +203,83 @@ live_design! {
         }
     }
 
+    Options = <Frame> {
+        walk: {width: Fill, height: Fit}
+        layout: {flow: Right, align: {x: 0.0, y: 0.5}, spacing: 10.0, padding: {left: 40., right: 40.}}
+
+        <IconWithTwoLineText> {
+            image = { image: (TAOBAO_COINS_IMG) }
+            caption = { label: "Taobao" }
+            caption2 = { label: "Coins" }
+        }
+
+        <FillerX> {}
+
+        <IconWithTwoLineText> {
+            image = { image: (HELP_CENTER_IMG) }
+            caption = { label: "Help" }
+            caption2 = { label: "Center" }
+        }
+
+        <FillerX> {}
+
+        <IconWithTwoLineText> {
+            image = { image: (LOGISTICS_IMG) }
+            caption = { label: "Logistics" }
+            caption2 = { label: "Center" }
+        }
+
+        <FillerX> {}
+
+        <IconWithTwoLineText> {
+            image = { image: (PAYMENTS_IMG) }
+            caption = { label: "Payments" }
+            caption2 = { label: "Center" }
+        }
+
+        <FillerX> {}
+
+        <IconWithTwoLineText> {
+            image = { image: (GLOBAL_DEALS_IMG) }
+            caption = { label: "Global" }
+            caption2 = { label: "Deals" }
+        }
+    }
+
+    Payment = <Box> {
+        walk: {width: Fill, height: Fit, margin: 10.0}
+        layout: {flow: Right, align: {x: 0.0, y: 0.5}, padding: {left: 10., right: 10.} spacing: 6.0}
+        draw_bg: {
+            color: #fff,
+            radius: 4.
+        }
+
+        <Label> {
+            walk: {width: Fit, height: Fit, margin: {top: 20., bottom: 20.}}
+            label: "Payment Support"
+            draw_label: {
+                color: #000
+                text_style: <REGULAR_TEXT> {font_size: 10.0},
+            }
+        }
+
+        <Image> {
+            walk: {width: 118, height: 18}
+            image: (CREDIT_CARDS_IMG)
+        }
+
+        <FillerX> {}
+
+        <LineH> {
+            walk: {width: 2.0, height: Fill}
+        }
+
+        <IconWithText> {
+            image = { image: (SHIPPING_ESTIMATE_IMG), walk: {width: 32, height: 32} }
+            caption = { label: "Ship Est" }
+        }
+    }
+
     HomeScreen = <Frame> {
         walk: {width: Fill, height: Fill}
         layout: {flow: Down}
@@ -148,43 +293,10 @@ live_design! {
             layout: {flow: Right, spacing: 6.0, padding: 0}
         }
 
-        top_bar = <Frame> {
-            walk: {width: Fill, height: Fit}
-            layout: {flow: Right, align: {x: 0.0, y: 0.5}, spacing: 10.0, padding: {left: 10., right: 10.}}
-
-            <IconWithText> {
-                image = { image: (WORLDWIDE_ICON) }
-                caption = { label: "WW" }
-            }
-
-            <FillerX> {}
-
-            <Label> {
-                walk: {width: Fit, height: Fit}
-                label: "Recommend"
-                draw_label: {
-                    color: #000
-                    text_style: <REGULAR_TEXT> {},
-                }
-            }
-            <Label> {
-                walk: {width: Fit, height: Fit}
-                label: "Special Offer"
-                draw_label: {
-                    color: #000
-                    text_style: <REGULAR_TEXT> {},
-                }
-            }
-
-            <FillerX> {}
-
-            <IconWithText> {
-                image = { image: (MEMBERSHIP_CODE_ICON) }
-                caption = { label: "M Code" }
-            }
-        }
-
+        <TopBar> {}
         <SearchBar> {}
+        <Options> {}
+        <Payment> {}
     }
 
 
